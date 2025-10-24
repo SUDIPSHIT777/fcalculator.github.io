db7148948e7e353ef9f3c283825d83b1832393af
SUDIPSHIT777 <sudipshit777@gmail.com>
2025-10-24 18:48:01 +0530

    Rebuild

---
+++ JAVASCRIPT/calculator.js
@@ -0,0 +1,18 @@
+let display = "";
+let result = document.getElementById("r-box");
+function clicks(val) {
+    switch (val) {
+        case "Ac":
+            display = "";
+            break;
+        case "del":
+            display = display.slice(0, -1);
+            break;
+        case "=":
+            display = eval(display.replaceAll("X", "*"));
+            break;
+        default:
+            display += val;
+    }
+    result.textContent = display;
+}
